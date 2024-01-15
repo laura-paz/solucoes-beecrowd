4400cf223fb74b0c855a01a0bfc3c82ff7c2a44b
Laura Paz <laura.paz.dev@gmail.com>
2024-01-15 14:58:35 -0300

    1007

---
+++ 1007.py
@@ -0,0 +1,7 @@
+valorA = int(input("Digite o valor A: "))
+valorB = int(input("Digite o valor B: "))
+valorC = int(input("Digite o valor C: "))
+valorD = int(input("Digite o valor D: "))
+diferenca = (valorA*valorB-valorC*valorD)
+
+print (f"DIFERENÇA = {diferenca}")
